05723b96c1f3acb24f2c9e9ed729399613c6dd08
SewerynMajewski <seweryn.majewski@gmail.com>
2025-03-25 17:40:33 +0100

    Boilerplate code for routing.js

--- routing/routing.js
+++ routing/routing.js
@@ -1,7 +1,37 @@
 // 📦 Zaimportuj moduł odpowiedzialne za routing poszczególnych części aplikacji.
 // 📦 Zaimportuj obiekt STATUS_CODE.
+const {STATUS_CODE} = require('../constants/statusCode');
 
 // 🏗 Stwórz tutaj funkcję 'requestRouting', która będzie obsługiwać zapytania HTTP.
+function requestRouting(request, response){
+    const {url, method} = request;
+    console.log('INFO (${new Date().toUTCString()}): ${method} - ${url})');
+
+    if (url === '/'){
+        return homeRouting(method, response);
+    }
+
+    if (url.startsWith('/product')){
+        return productRouting(request, response);
+    }
+
+    if (url === '/logout'){
+        return logoutRouting(method, response);
+    }
+
+    if (url === '/kill'){
+        response.statusCode = STATUS_CODE.FOUND;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html><head><title>Kill app</title></head><body><h1>Killed application</h1></body></html>');
+    }
+
+    response.statusCode = STATUS_CODE.NOT_FOUND;
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html><head><title>404</title></head><body><h1>404 - Not Found</h1></body></html>');
+    response.end();
+
+    console.warn(`ERROR (${new Date().toUTCString()}): Page not found ${url})`)
+}
 // Podpowiedź: const requestRouting = (request, response) => {
 // 🏗 Tutaj stwórz logowanie do konsoli informacji, mówiące o typie logowania (INFO), dacie, metodzie oraz url żądania.
 // 🏗 Tutaj stwórz podstawowy 'request routing' dla ścieżek '/', zawierającej /product' oraz '/logout'. Przekaż `request` i `routing` do odpowiednio routingu.
